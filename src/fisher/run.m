7043f40e525530a62384e57b387b9d9996b65caa
Nuullll <vfirst218@gmail.com>
2018-06-08 00:59:02 +0800

    Fisher's rule complete

--- src/fisher/run.m
+++ src/fisher/run.m
@@ -1,6 +1,6 @@
 % fisher
 %% specify samples
-situation = 1;
+situation = 2;
 
 if situation == 1
     w1 = [
@@ -47,3 +47,34 @@ m2 = mean(w2);
 Sw = size(w1,1) * cov(w1,1) + size(w2,1) + cov(w2,1);
 W = Sw \ (m1-m2)';
 W = W / norm(W);
+
+%% plot
+% target line equation:
+% y = k * x
+plot([-5 5],[-5 5]*W(2)/W(1));
+
+% projected samples
+w1p = w1 * (W * W');
+w2p = w2 * (W * W');
+
+scatter(w1p(:,1),w1p(:,2),50,'r^','filled');
+scatter(w2p(:,1),w2p(:,2),50,'b^','filled');
+
+% projection lines
+n1 = size(w1,1);
+t = [w1(:),w1p(:)]';
+plot(t(:,1:n1),t(:,n1+1:end),'r--');
+
+n2 = size(w2,1);
+t = [w2(:),w2p(:)]';
+plot(t(:,1:n2),t(:,n2+1:end),'b--');
+
+axis equal;
+legend('类别1','类别2','投影方向','类别1（降维）','类别2（降维）');
+
+% boundary
+m1p = mean(w1p);
+m2p = mean(w2p);
+x = linspace(-5,5);
+y = -W(1)/W(2)*(x-(m1p(1)+m2p(1))/2) + (m1p(2)+m2p(2))/2;
+line(x,y);
